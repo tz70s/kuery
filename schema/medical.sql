a026b4ec05d257aac97a20cfee3437d44969d0aa
tz70s <su3g4284zo6y7@gmail.com>
2018-06-17 02:23:14 +0800

    Use mysqldump instead of phpadmin dump file.

--- schema/medical.sql
+++ schema/medical.sql
@@ -1,125 +1,105 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.21, for osx10.13 (x86_64)
 --
--- 主機: localhost
--- 產生時間： 2018 年 06 月 14 日 19:23
--- 伺服器版本: 10.1.28-MariaDB
--- PHP 版本： 7.1.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: medical
+-- ------------------------------------------------------
+-- Server version	5.7.1-TiDB-v1.0.5-7-g6c2fe07
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- 資料庫： ` medical`
+-- Table structure for table `hospital`
 --
 
--- --------------------------------------------------------
-
---
--- 資料表結構 `hospital`
---
-
+DROP TABLE IF EXISTS `hospital`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `hospital` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `level` enum('national','local') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_bin AUTO_INCREMENT=30002;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 資料表的匯出資料 `hospital`
+-- Dumping data for table `hospital`
 --
 
-INSERT INTO `hospital` (`id`, `name`, `level`) VALUES
-(1, '宏孕診所', 'local'),
-(2, '靚漾診所', 'local'),
-(3, '琦美診所', 'local'),
-(4, '費洛佳皮膚科', 'local'),
-(5, '費名耳鼻喉科', 'local'),
-(6, '衛生福利部臺北醫院', 'national'),
-(7, '衛生福利部雙和醫院', 'national'),
-(8, '衛生福利部桃園醫院', 'national'),
-(9, '衛生福利部苗栗醫院', 'national'),
-(10, '衛生福利部臺中醫院', 'national');
-
--- --------------------------------------------------------
+LOCK TABLES `hospital` WRITE;
+/*!40000 ALTER TABLE `hospital` DISABLE KEYS */;
+INSERT INTO `hospital` VALUES (1,'宏孕診所','local'),(2,'靚漾診所','local'),(3,'琦美診所','local'),(4,'費洛佳皮膚科','local'),(5,'費名耳鼻喉科','local'),(6,'衛生福利部臺北醫院','national'),(7,'衛生福利部雙和醫院','national'),(8,'衛生福利部桃園醫院','national'),(9,'衛生福利部苗栗醫院','national'),(10,'衛生福利部臺中醫院','national');
+/*!40000 ALTER TABLE `hospital` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 資料表結構 `medical_personnel`
+-- Table structure for table `medical_personnel`
 --
 
+DROP TABLE IF EXISTS `medical_personnel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `medical_personnel` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `job` enum('nurse','pharmacist','doctor') NOT NULL,
   `hospital_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`hospital_id`) REFERENCES `hospital`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `hospital_id` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_bin AUTO_INCREMENT=30002;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 資料表的匯出資料 `medical_personnel`
+-- Dumping data for table `medical_personnel`
 --
 
-INSERT INTO `medical_personnel` (`id`, `name`, `job`, `hospital_id`) VALUES
-(1, '張三', 'nurse', 3),
-(2, '張四', 'pharmacist', 4),
-(3, '李五', 'nurse', 2),
-(4, '李六', 'pharmacist', 2),
-(5, '廖偉倫', 'pharmacist', 1),
-(6, '鄭宗翰', 'nurse', 3),
-(7, '王淑苓', 'nurse', 4),
-(8, '蔡佩瑋', 'nurse', 4),
-(9, '吳武梅', 'doctor', 1),
-(10, '宋法慧', 'doctor', 2),
-(11, '陳宗斌', 'pharmacist', 1),
-(12, '吳漢琇', 'nurse', 1),
-(13, '吳明亨', 'doctor', 3),
-(14, '李宗翰', 'doctor', 4),
-(15, '郭惠萍', 'nurse', 2),
-(16, '楊惠芳', 'pharmacist', 3),
-(17, '黃慧全', 'nurse', 1);
-
--- --------------------------------------------------------
+LOCK TABLES `medical_personnel` WRITE;
+/*!40000 ALTER TABLE `medical_personnel` DISABLE KEYS */;
+INSERT INTO `medical_personnel` VALUES (1,'張三','nurse',3),(2,'張四','pharmacist',4),(3,'李五','nurse',2),(4,'李六','pharmacist',2),(5,'廖偉倫','pharmacist',1),(6,'鄭宗翰','nurse',3),(7,'王淑苓','nurse',4),(8,'蔡佩瑋','nurse',4),(9,'吳武梅','doctor',1),(10,'宋法慧','doctor',2),(11,'陳宗斌','pharmacist',1),(12,'吳漢琇','nurse',1),(13,'吳明亨','doctor',3),(14,'李宗翰','doctor',4),(15,'郭惠萍','nurse',2),(16,'楊惠芳','pharmacist',3),(17,'黃慧全','nurse',1);
+/*!40000 ALTER TABLE `medical_personnel` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- 資料表結構 `pharmacy`
+-- Table structure for table `pharmacy`
 --
 
+DROP TABLE IF EXISTS `pharmacy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pharmacy` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `hospital_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`hospital_id`) REFERENCES `hospital`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `hospital_id` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_bin AUTO_INCREMENT=30002;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- 資料表的匯出資料 `pharmacy`
+-- Dumping data for table `pharmacy`
 --
 
-INSERT INTO `pharmacy` (`id`, `name`, `hospital_id`) VALUES
-(1, '易昌大藥局', 1),
-(2, '南和藥局 ', 2),
-(3, '日生大藥局 ', 3),
-(4, '三元藥局', 4),
-(5, '大安藥局', 5),
-(6, '上海聯合藥局', 6),
-(7, '中心藥局 ', 7),
-(8, '楊藥局 ', 8),
-(9, '和康大藥局', 9),
-(10, '新獻安藥局', 10);
+LOCK TABLES `pharmacy` WRITE;
+/*!40000 ALTER TABLE `pharmacy` DISABLE KEYS */;
+INSERT INTO `pharmacy` VALUES (1,'易昌大藥局',1),(2,'南和藥局 ',2),(3,'日生大藥局 ',3),(4,'三元藥局',4),(5,'大安藥局',5),(6,'上海聯合藥局',6),(7,'中心藥局 ',7),(8,'楊藥局 ',8),(9,'和康大藥局',9),(10,'新獻安藥局',10);
+/*!40000 ALTER TABLE `pharmacy` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-17  2:20:30
