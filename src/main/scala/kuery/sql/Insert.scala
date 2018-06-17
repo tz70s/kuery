b531b199383e44ecdbad8ee95817594cf424843f
tz70s <su3g4284zo6y7@gmail.com>
2018-06-17 22:59:46 +0800

    Insert statement completion in SQL..

--- src/main/scala/kuery/sql/Insert.scala
+++ src/main/scala/kuery/sql/Insert.scala
@@ -19,6 +19,9 @@ package kuery.sql
 import akka.http.scaladsl.server.Route
 import kuery.model._
 import akka.http.scaladsl.server.Directives._
+import slick.jdbc.MySQLProfile.api._
+
+import scala.concurrent.Await
 
 trait InsertService {
   this: Sequelizer =>
@@ -27,19 +30,38 @@ trait InsertService {
    * Insert of hospital, transform directly into sql statement and ignore the id value.
    * INSERT (name, level) INTO `hospital` VALUES (name, level);
    */
-  implicit def insertHospital(hospital: Hospital): Route = complete(hospital.toString)
+  implicit def insertHospital(hospital: Hospital): Route = {
+
+    val statement = DBIO.seq(HospitalTable.query += hospital)
+
+    val _ = Await.result(db.run(statement), timeout)
+
+    complete("Ok")
+  }
 
   /**
    * Unlike hospital, personnel has the foreign key to hospital identification.
    * Before insertion, user might need to first discover the hospital ident by specific condition matching.
    * TODO: We may consider to support automatically match this by opening two sql statements transactionally.
    */
-  implicit def insertPersonnel(personnel: Personnel): Route = complete(personnel.toString)
+  implicit def insertPersonnel(personnel: Personnel): Route = {
+    val statement = DBIO.seq(PersonnelTable.query += personnel)
+
+    val _ = Await.result(db.run(statement), timeout)
+
+    complete("Ok")
+  }
 
   /**
    * Unlike hospital, pharmacy has the foreign key to hospital identification.
    * Before insertion, user might need to first discover the hospital ident by specific condition matching.
    * TODO: We may consider to support automatically match this by opening two sql statements transactionally.
    */
-  implicit def insertPharmacy(pharmacy: Pharmacy): Route = complete(pharmacy.toString)
+  implicit def insertPharmacy(pharmacy: Pharmacy): Route = {
+    val statement = DBIO.seq(PharmacyTable.query += pharmacy)
+
+    val _ = Await.result(db.run(statement), timeout)
+
+    complete("Ok")
+  }
 }
